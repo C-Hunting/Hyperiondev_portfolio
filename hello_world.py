2a55d3c70fbf840dac2770028eb708b716871945
C-Hunting <128845450+C-Hunting@users.noreply.github.com>
2023-05-24 19:55:17 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+name = input ("What is your name?")
+print ("Hello " + name + " !") 
+age = input ("What's your age " + name + "?")
+print ("So you are " + age + " " + name + ", cool!")
+print ("Hello World!")
